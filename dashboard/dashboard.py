cb27e54281c9e25fdf7d5831344f0cf8f3552724
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-16 17:50:14 +0100

    init dashboard

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,3 +8,17 @@ from dash.dependencies import Input,Output
 import plotly.express as px
 
 from datetime import datetime
+
+
+class SalesDashboard:
+    def __init__(self,output_dir="/Users/ilyasdahhou/Desktop/ecom-front/ecom/spark_processing/notebooks/output"):
+        self.output_dir=output_dir
+        self.app=dash.Dash(__name__)
+        self.setup_layout()
+        self.setup_callbacks()
+
+    def load_data(self, start_date, end_date):
+        """
+        Charge les données des fichiers entre start_date et end_date
+        """
+        all_data = []
